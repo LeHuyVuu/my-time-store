965f6439ba7fee9508cb9bed02792d59e81e1749
TranPhuKhag <167117602+TranPhuKhag@users.noreply.github.com>
2024-11-18 15:50:45 +0700

    Update existing mapper and error code files

--- My Time Store Back End/src/main/java/com/boboibo/mytimestore/exception/ErrorCode.java
+++ My Time Store Back End/src/main/java/com/boboibo/mytimestore/exception/ErrorCode.java
@@ -1,4 +1,21 @@
 package com.boboibo.mytimestore.exception;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum ErrorCode {
+    ;
+
+    int code;
+    String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
